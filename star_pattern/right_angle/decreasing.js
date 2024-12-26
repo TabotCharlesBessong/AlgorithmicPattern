62cf5218b01494e1d37b15764f960162427bbdcc
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-26 06:14:41 +0100

    increasing and descreasing right angle triangle

---
+++ star_pattern/right_angle/decreasing.js
@@ -0,0 +1,12 @@
+const decreasing = (n) => {
+  for (let i = n; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      process.stdout.write("*  ");
+    }
+    process.stdout.write("\n");
+  }
+};
+
+const size = 5;
+
+decreasing(size);

baeb69b55e8a1dd017e5164a2f27fa58aa4d8381
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-12-30 05:27:54 +0100

    middle node of a linked list

---
+++ fast_slow/middle_node/solution.js
@@ -0,0 +1,12 @@
+const middleNode = (head) => {
+  if (!head && !head.next) return head
+  let slow = head;
+  let fast = head;
+  
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}

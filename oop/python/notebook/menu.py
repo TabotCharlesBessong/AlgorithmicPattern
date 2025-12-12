d51e2df385a911d8006f209c66d05fe3935763c2
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-12-12 15:31:55 +0100

    refactor search algorithms for improved efficiency and readability

---
+++ oop/python/notebook/menu.py
@@ -0,0 +1,76 @@
+
+from notebook import Notebook
+from note import Note
+
+class Menu:
+  '''Display a menu and handle input commands.'''
+  def __init__(self):
+    self.notebook = Notebook()
+    self.choices = {
+      "1": self.show_notes,
+      "2": self.search_notes,
+      "3": self.add_note,
+      "4": self.modify_note,
+      "5": self.quit
+    }
+    
+  def display_menu(self):
+    print("""
+    Notebook Menu
+    -------------
+    1. Show all Notes
+    2. Search Notes
+    3. Add Note
+    4. Modify Note
+    5. Quit
+    """)
+    
+  def run(self):
+    '''Display the menu and handle user input.'''
+    while True:
+      self.display_menu()
+      choice = input("Enter an option: ")
+      action = self.choices.get(choice)
+      if action:
+        action()
+      else:
+        print("{0} is not a valid choice".format(choice))
+        
+  def show_notes(self):
+    '''Show all notes.'''
+    for note in self.notebook.notes:
+      print("{0}: {1}\n{2}".format(note.id, note.tags, note.memo))
+      
+  def search_notes(self):
+    '''Search for notes.'''
+    filter = input("Search for: ")
+    notes = self.notebook.search(filter)
+    for note in notes:
+      print("{0}: {1}\n{2}".format(note.id, note.tags, note.memo))
+      
+      
+  def add_note(self):
+    '''Add a new note.'''
+    memo = input("Enter a memo: ")
+    self.notebook.new_note(memo)
+    print("Note added")
+    
+  def modify_note(self):
+    '''Modify a note.'''
+    id = input("Enter a note id: ")
+    memo = input("Enter a new memo: ")
+    tags = input("Enter new tags: ")
+    # self.notebook.modify_note(id, memo, tags)
+    if memo:
+      self.notebook.modify_memo(id, memo)
+    if tags:
+      self.notebook.modify_tags(id, tags)
+      
+  def quit(self):
+    '''Quit the program.'''
+    print("Thank you for using your notebook today.")
+    exit(0)
+    
+if __name__ == "__main__":
+  menu = Menu()
+  menu.run()
